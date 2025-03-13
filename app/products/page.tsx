e1e25c437eae2d0543f579d1b2d31253211af90a
code-verse <m.black.wolf.k@gmail.com>
2025-03-13 15:34:01 +0330

    edit Products page & create InputSearch

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,6 +6,7 @@ import Loading from "./Loading";
 import ProductsBody from "@/components/productsPage/ProductsBody";
 import Pagination from "@/components/common/Pagination";
 import NotFoundSearch from "@/components/common/NotFoundSearch";
+import InputSearch from "@/components/productsPage/InputSearch";
 
 interface TotalProducts {
   totalProducts: number;
@@ -63,20 +64,8 @@ const Products = async ({
           <div className="flex flex-col md:flex-row items-center justify-between gap-y-2 md:gap-y-0 mb-5 md:mb-12">
             <h2 className="section-title">محصولات</h2>
 
-            <div className="flex items-center justify-center py-1 px-2.5 text-zinc-700 dark:text-white bg-white dark:bg-zinc-700 rounded-xl border border-gray-300">
-              <svg className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6">
-                <use href="#magnifying-glass"></use>
-              </svg>
-              <input
-                className="w-80 py-1 px-2.5 text-sm md:text-base lg:text-lg bg-white dark:bg-zinc-700 outline-none"
-                type="text"
-                maxLength={20}
-                placeholder="جستجوی محصول"
-                // defaultValue={paramSearch === "undefined" ? "" : paramSearch}
-                // value={paramSearch === "undefined" ? "" : paramSearch}
-                //   onChange={handleInputChange}
-              />
-            </div>
+            {/* input search */}
+            <InputSearch />
           </div>
 
           {/* <!-- Section Body  --> */}
---
+++ components/productsPage/InputSearch.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+const InputSearch = () => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [search, setSearch] = useState(searchParams.get("search") || "");
+  const [showSearch, setShowSearch] = useState(true);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchValue = e.target.value;
+
+    const pattern = /^[\u0600-\u06FF0-9\s]*$/;
+    if (pattern.test(searchValue) && searchValue.length <= 30) {
+      setSearch(searchValue);
+    }
+    if (!searchValue) {
+      const params = new URLSearchParams(searchParams);
+      params.delete("search");
+
+      router.replace(`${pathname}?${params}`);
+      setShowSearch(true);
+    }
+  };
+
+  const handleClickSearch = () => {
+    if (search !== "") {
+      const params = new URLSearchParams(searchParams);
+      params.set("search", search);
+      params.set("page", "1");
+
+      router.replace(`${pathname}?${params}`);
+
+      setShowSearch(false);
+    }
+  };
+
+  const handleDelete = () => {
+    if (search !== "") {
+      const params = new URLSearchParams(searchParams);
+      params.delete("search");
+      params.set("page", "1");
+
+      router.replace(`${pathname}?${params}`);
+      setSearch("");
+      setShowSearch(true);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center relative">
+      <input
+        className="w-80 h-8 lg:h-10 pr-2 pl-8 text-sm md:text-base lg:text-lg text-zinc-700 dark:text-white bg-white dark:bg-zinc-700 rounded-lg border border-gray-400 focus:border-orange-300 transition-colors duration-150 outline-none"
+        type="text"
+        maxLength={50}
+        placeholder="جستجوی محصول"
+        value={search}
+        onChange={handleInputChange}
+      />
+      {showSearch ? (
+        <button
+          type="button"
+          onClick={handleClickSearch}
+          className="absolute top-2 left-2 text-zinc-700 dark:text-white hover:text-orange-300 transition-colors duration-150"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+        </button>
+      ) : (
+        <button
+          type="button"
+          onClick={handleDelete}
+          className="absolute top-2 left-2 text-zinc-700 dark:text-white hover:text-orange-300 transition-colors duration-150"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default InputSearch;
