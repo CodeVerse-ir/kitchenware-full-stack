6196f7a922acefbc01b3b76bf7fdb802a542c7d2
code-verse <m.black.wolf.k@gmail.com>
2025-03-13 13:08:19 +0330

    edit ProductsCategory

--- components/homePage/ProductsCategory.tsx
+++ components/homePage/ProductsCategory.tsx
@@ -26,42 +26,34 @@ const category = [
 
 const ProductsCategory = async () => {
   return (
-    <>
-      {category && (
-        <>
-          <section className="products-category mb-10 md:mb-20">
-            <div className="container">
-              {/* <!-- Section Body --> */}
-              <div className="flex flex-wrap items-center gap-6 md:flex-nowrap justify-evenly">
-                {/* <!-- items --> */}
-                {category.map((item, index) => {
-                  return (
-                    <Link
-                      key={index}
-                      href={`/products?category=${encodeURIComponent(
-                        item.text
-                      )}`}
-                      className="max-w-[6.25rem] lg:max-w-[12.5rem] flex flex-col"
-                    >
-                      <Image
-                        className="lg:w-48 lg:h-56"
-                        src={item.image}
-                        alt={`item ${index + 1}`}
-                        width={100}
-                        height={128}
-                      ></Image>
-                      <sub className="lg:text-xl/7 font-DanaBold text-sm/5 text-zinc-700 dark:text-white md:mt-2.5 mt-1.5 text-center">
-                        {item.text}
-                      </sub>
-                    </Link>
-                  );
-                })}
-              </div>
-            </div>
-          </section>
-        </>
-      )}
-    </>
+    <section className="products-category mb-10 md:mb-20">
+      <div className="container">
+        {/* <!-- Section Body --> */}
+        <div className="flex flex-wrap items-center gap-6 md:flex-nowrap justify-evenly">
+          {/* <!-- items --> */}
+          {category.map((item, index) => {
+            return (
+              <Link
+                key={index}
+                href={`/products?category=${encodeURIComponent(item.text)}`}
+                className="max-w-[6.25rem] lg:max-w-[12.5rem] flex flex-col"
+              >
+                <Image
+                  className="lg:w-48 lg:h-56"
+                  src={item.image}
+                  alt={`item ${index + 1}`}
+                  width={100}
+                  height={128}
+                ></Image>
+                <sub className="lg:text-xl/7 font-DanaBold text-sm/5 text-zinc-700 dark:text-white md:mt-2.5 mt-1.5 text-center">
+                  {item.text}
+                </sub>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </section>
   );
 };
 
