f7b208bdf9aaef4bbc081467dd7004f35790f5fc
code-verse <m.black.wolf.k@gmail.com>
2025-03-27 15:58:41 +0330

    edit Image in Biography

--- components/layoutProfile/Biography.tsx
+++ components/layoutProfile/Biography.tsx
@@ -9,7 +9,7 @@ const Biography = () => {
   return (
     <div className="flex items-center justify-start w-2/4 lg:w-72 p-5 gap-x-2.5 text-black dark:text-white bg-white dark:bg-zinc-700 shadow-normal rounded-2xl select-none">
       <Image
-        className="w-14 h-14 md:w-16 md:h-16 lg:w-20 lg:h-20 p-1 border border-gray-300 rounded-full"
+        className="size-14 md:size-16 lg:size-20 p-1 border border-gray-300 rounded-full"
         src={user?.image || "/image/comment/avatar.png"}
         alt="avatar"
         width={80}
@@ -17,7 +17,7 @@ const Biography = () => {
         sizes="(min-width: 768px)"
         priority
       />
-      <h4 className="font-MorabbaLight text-lg md:text-xl lg:text-2xl">
+      <h4 className="flex-1 font-MorabbaLight text-lg md:text-xl lg:text-2xl">
         {user?.first_name} {user?.last_name}
       </h4>
     </div>
