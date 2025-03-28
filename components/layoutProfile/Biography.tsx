932e9fa985ab01cd600cf0505cb9b5b9d74d2b19
code-verse <m.black.wolf.k@gmail.com>
2025-03-28 13:50:34 +0330

    edit Biography

--- components/layoutProfile/Biography.tsx
+++ components/layoutProfile/Biography.tsx
@@ -7,7 +7,7 @@ const Biography = () => {
   const { user } = useSession();
 
   return (
-    <div className="flex items-center justify-start w-2/4 lg:w-72 p-5 gap-x-2.5 text-black dark:text-white bg-white dark:bg-zinc-700 shadow-normal rounded-2xl select-none">
+    <div className="flex flex-col items-center justify-center w-2/4 lg:w-72 p-5 gap-x-2.5 text-black dark:text-white bg-white dark:bg-zinc-700 shadow-normal rounded-2xl select-none">
       <Image
         className="size-14 md:size-16 lg:size-20 p-1 border border-gray-300 rounded-full"
         src={user?.image || "/image/comment/avatar.png"}
@@ -17,7 +17,7 @@ const Biography = () => {
         sizes="(min-width: 768px)"
         priority
       />
-      <h4 className="flex-1 font-MorabbaLight text-lg md:text-xl lg:text-2xl">
+      <h4 className="flex-1 text-center font-MorabbaLight text-lg md:text-xl lg:text-2xl">
         {user?.first_name} {user?.last_name}
       </h4>
     </div>
